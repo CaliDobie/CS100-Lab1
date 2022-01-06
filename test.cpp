52ae77f827c994f9c9f6c51c76283008197581ba
CaliDobie <carel009@ucr.edu>
2022-01-05 20:04:09 -0800

    Testing edge casese and submission

--- test.cpp
+++ test.cpp
@@ -7,6 +7,26 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, Hello) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "hello";
+    EXPECT_EQ("hello",echo(2,test_val));
+}
+
+TEST(EchoTest, Space) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " ";
+    EXPECT_EQ(" ",echo(2,test_val));
+}
+
+TEST(EchoTest, Bye) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "bye";
+    EXPECT_EQ("bye",echo(2,test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
